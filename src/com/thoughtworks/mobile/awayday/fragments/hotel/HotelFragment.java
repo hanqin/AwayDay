14dd5c07b0905748bb23033a74ad09ff946d692f
Han Qin <hanhaify@msn.com>
2012-07-16 16:20:39 +0800

    Han: add event to local calendar

--- src/com/thoughtworks/mobile/awayday/fragments/hotel/HotelFragment.java
+++ src/com/thoughtworks/mobile/awayday/fragments/hotel/HotelFragment.java
@@ -1,9 +1,7 @@
 package com.thoughtworks.mobile.awayday.fragments.hotel;
 
 import android.app.Fragment;
-import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,12 +9,8 @@ import android.view.ViewGroup;
 import android.widget.*;
 import com.thoughtworks.mobile.awayday.R;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import static android.graphics.drawable.Drawable.createFromStream;
-import static android.widget.LinearLayout.VERTICAL;
-import static com.thoughtworks.mobile.awayday.util.DensityUtil.toPx;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class HotelFragment extends Fragment {
 
@@ -25,70 +19,31 @@ public class HotelFragment extends Fragment {
         final View resultView = inflater.inflate(R.layout.hotel_details, null);
         final Gallery gallery = (Gallery) resultView.findViewById(R.id.event_gallery);
         gallery.setAdapter(new ImageAdapter());
+
+        final View reminder = resultView.findViewById(R.id.hotel_details_reminder_bar);
+        reminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    addEventToCalendar();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         return resultView;
     }
 
-    private static class ImageAdapter extends BaseAdapter {
-
-        public static final int DEFAULT_THUMBNAIL_WIDTH_IN_DP = 270;
-        public static final int DEFAULT_THUMBNAIL_HEIGHT_IN_DP = 216;
-
-        @Override
-        public int getCount() {
-            return 3;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return position;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView resultView;
-            if (convertView == null) {
-                resultView = new ImageView(parent.getContext());
-            } else {
-                resultView = (ImageView) convertView;
-            }
-            String fileName = "hotel/thumbs/hotel_" + position + ".jpg";
-
-            final Drawable imageDrawable = getDrawableOf(parent.getContext(), fileName);
-            resultView.setImageDrawable(imageDrawable);
-            resultView.setPadding(10, 10, 10, 10);
-            resultView.setBackgroundColor(Color.WHITE);
-
-            final int width = toPx(parent.getResources(), DEFAULT_THUMBNAIL_WIDTH_IN_DP);
-            final int height = toPx(parent.getResources(), DEFAULT_THUMBNAIL_HEIGHT_IN_DP);
-            resultView.setLayoutParams(new Gallery.LayoutParams(width, height));
-            resultView.setScaleType(ImageView.ScaleType.FIT_XY);
-
-            return resultView;
-        }
-
-        private Drawable getDrawableOf(Context context, String fileName) {
-            InputStream stream = null;
-            try {
-                stream = context.getAssets().open(fileName);
-                return createFromStream(stream, null);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (stream != null) {
-                    try {
-                        stream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            return null;
-        }
+    protected void addEventToCalendar() throws ParseException {
+        Intent intent = new Intent(Intent.ACTION_EDIT);
+        intent.setType("vnd.android.cursor.item/event");
+        final String dateTimePattern = "MMMM d, hh:mm a";
+        intent.putExtra("beginTime", new SimpleDateFormat(dateTimePattern).parse("September 14, 12:00 PM").getTime());
+        intent.putExtra("endTime", new SimpleDateFormat(dateTimePattern).parse("September 14, 14:00 PM").getTime());
+        intent.putExtra("title", "Hotel Check in");
+        intent.putExtra("eventLocation", "");
+        intent.putExtra("description", "Check in Conference Resort Chengdu:  www.hojoqc.com, then take a small break");
+        getActivity().startActivity(intent);
     }
 
 }
---
+++ src/com/thoughtworks/mobile/awayday/fragments/hotel/ImageAdapter.java
@@ -0,0 +1,79 @@
+package com.thoughtworks.mobile.awayday.fragments.hotel;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static android.graphics.drawable.Drawable.createFromStream;
+import static com.thoughtworks.mobile.awayday.util.DensityUtil.toPx;
+
+class ImageAdapter extends BaseAdapter {
+
+    public static final int DEFAULT_THUMBNAIL_WIDTH_IN_DP = 270;
+    public static final int DEFAULT_THUMBNAIL_HEIGHT_IN_DP = 216;
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView resultView;
+        if (convertView == null) {
+            resultView = new ImageView(parent.getContext());
+        } else {
+            resultView = (ImageView) convertView;
+        }
+        String fileName = "hotel/thumbs/hotel_" + position + ".jpg";
+
+        final Drawable imageDrawable = getDrawableOf(parent.getContext(), fileName);
+        resultView.setImageDrawable(imageDrawable);
+        resultView.setPadding(10, 10, 10, 10);
+        resultView.setBackgroundColor(Color.WHITE);
+
+        final int width = toPx(parent.getResources(), DEFAULT_THUMBNAIL_WIDTH_IN_DP);
+        final int height = toPx(parent.getResources(), DEFAULT_THUMBNAIL_HEIGHT_IN_DP);
+        resultView.setLayoutParams(new Gallery.LayoutParams(width, height));
+        resultView.setScaleType(ImageView.ScaleType.FIT_XY);
+
+        return resultView;
+    }
+
+    private Drawable getDrawableOf(Context context, String fileName) {
+        InputStream stream = null;
+        try {
+            stream = context.getAssets().open(fileName);
+            return createFromStream(stream, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+}
