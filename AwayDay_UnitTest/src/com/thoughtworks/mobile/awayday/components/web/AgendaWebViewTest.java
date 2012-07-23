57bc2ad91b917bfd55ead82e5b9783e0b1388532
Han Qin <hanhaify@msn.com>
2012-07-23 11:52:07 +0800

    Han: init the test module

---
+++ AwayDay_UnitTest/src/com/thoughtworks/mobile/awayday/components/web/AgendaWebViewTest.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.mobile.awayday.components.web;
+
+import org.junit.Test;
+
+
+public class AgendaWebViewTest {
+
+    @Test
+    public void should_load_agenda_from_html() {
+        System.out.println("true = " + true);
+    }
+}
